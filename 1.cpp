d0465358a2d5f13eb278aef89f942e920fdc888e
RafidImran <100108588+RafidImran@users.noreply.github.com>
2022-11-22 19:46:41 +0500

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+
+{
+    srand(time(NULL));
+    int x ,i ;
+
+    for (i=0 ; i<3 ; i++)
+    {
+        x=rand()%9+1 ;
+        cout << x <<"\t" ;
+
+        for (int j=1 ; j<=x ; j++){
+         	cout << "* " ;
+        }
+	    
+   	 cout << endl ;
+    }
+	
+    return 0 ;
+
+}
